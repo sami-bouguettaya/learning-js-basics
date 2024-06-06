d677c866d16714d830e6dee0b1e465911e0b4d51
sami-bouguettaya <samibouguettaya66@gmail.com>
2024-06-06 20:15:35 +0100

    Working on projet_03

--- proj_03/index.js
+++ proj_03/index.js
@@ -1,23 +1,11 @@
 
-// let box = document.getElementById("box")
 
-// box.addEventListener("click" , function(){
-//     console.log("i want to open the box !")
-// })
+let myleads = [];
+const inputEl = document.getElementById("input-el");
+const inputBtn = document.getElementById("input-btn");
 
-let inputBtn = document.getElementById("input-btn")
-
-inputBtn.addEventListener("click", myFunction)
-   
-function myFunction() {
-    console.log("btn clicked")
-}
-
-
-
-
-// let inputBtn = document.getElementById("input-btn")
-
-// inputBtn.addEventListener("click" , function(){
-//     console.log("btn clicked frr")
-// })
+inputBtn.addEventListener("click", function() {
+    myleads.push(inputEl.value);
+    console.log(myleads);
+    inputEl.value = "";  // Clear the input field after saving the value
+});
