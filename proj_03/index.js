cecf437feba2cffa0fbd04a2a3d971fa37402a2d
sami-bouguettaya <samibouguettaya66@gmail.com>
2024-06-09 20:14:38 +0100

    some process

--- proj_03/index.js
+++ proj_03/index.js
@@ -1,11 +1,16 @@
 
 
-let myleads = [];
+let myleads = ["jejueudud","liytaouy252423", "2kyuftaiytfyf"];
 const inputEl = document.getElementById("input-el");
 const inputBtn = document.getElementById("input-btn");
+const ulEl = document.getElementById("ul-el");
 
 inputBtn.addEventListener("click", function() {
     myleads.push(inputEl.value);
     console.log(myleads);
     inputEl.value = "";  // Clear the input field after saving the value
 });
+
+for(let i = 0 ; i< myleads.length ; i++){
+    ulEl.innerHTML += "<li>"+ myleads[i] +"</li>";
+}
