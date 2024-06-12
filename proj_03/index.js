8468daf37c5a2435fd3a6e0977770df73af7ce48
sami-bouguettaya <samibouguettaya66@gmail.com>
2024-06-12 20:51:52 +0100

    integrate local storage

--- proj_03/index.js
+++ proj_03/index.js
@@ -1,24 +1,35 @@
 
 
-let myleads = [];
+let myLeads = [];
 const inputEl = document.getElementById("input-el");
 const inputBtn = document.getElementById("input-btn");
 const ulEl = document.getElementById("ul-el");
 
+let leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
+
+if (leadsFromLocalStorage) {
+  myLeads =leadsFromLocalStorage;
+  renderLeads();
+
+}
+
+
 inputBtn.addEventListener("click", function() {
-    myleads.push(inputEl.value);
-    inputEl.value = "";  
+    myLeads.push(inputEl.value);
+    inputEl.value = "";
+    
+    localStorage.setItem("myLeads" , JSON.stringify(myLeads))
     renderLeads();
 });
 
 
 function renderLeads(){
     let listeItems =""
-   for(let i = 0 ; i< myleads.length ; i++){
-    // listeItems += "<li><a target='_blank'href='"+ myleads[i]+"'>"+ myleads[i]+" </a> </li>"
+   for(let i = 0 ; i< myLeads.length ; i++){
+    // listeItems += "<li><a target='_blank'href='"+ myLeads[i]+"'>"+ myLeads[i]+" </a> </li>"
     listeItems +=   `<li>
-                         <a target='_blank'href='${myleads[i]}'>
-                           ${myleads[i]}
+                         <a target='_blank'href='${myLeads[i]}'>
+                           ${myLeads[i]}
                          </a> 
                      </li> 
                      `
